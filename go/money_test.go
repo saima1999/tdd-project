4fca35c840ef6c75c82e9ffe9ebbc93009a28852
Saima <saima.munir@hellofresh.ca>
2022-05-16 11:45:25 -0400

    addition feature for Moneys in the same currency

--- go/money_test.go
+++ go/money_test.go
@@ -31,9 +31,39 @@ func assertEqual(t *testing.T, expected Money, actual Money) {
 	}
 }
 
+func testAddition(t *testing.T) {
+	var portfolio Portfolio
+	var portfolioInDollars Money
+
+	fiveDollars := Money{amount: 5, currency: "USD"}
+	tenDollars := Money{amount: 10, currency: "USD"}
+	fifteenDollars := Money{amount: 15, currency: "USD"}
+
+	portfolio = portfolio.Add(fiveDollars)
+	portfolio = portfolio.Add(tenDollars)
+	portfolioInDollars = portfolio.Evaluate("USD")
+
+	assertEqual(t, fifteenDollars, portfolioInDollars)
+}
+
 type Money struct {
 	amount   float64
 	currency string
+}
+
+type Portfolio []Money
+
+func (p Portfolio) Add(money Money) Portfolio {
+	p = append(p, money)
+	return p
+}
+
+func (p Portfolio) Evaluate(currency string) Money {
+	total := 0.0
+	for _, m := range p {
+		total = total + m.amount
+	}
+	return Money{amount: total, currency: currency}
 }
 
 func (m Money) Times(multiplier int) Money {
